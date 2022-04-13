63cc36b41dbd17617bc8a5334d1fe12c3a9f6ee6
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-04-13 11:21:50 -0300

    Add files via upload

--- OrientacaoObjetos/src/Main.java
+++ OrientacaoObjetos/src/Main.java
@@ -3,16 +3,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Pessoa pessoal = new Pessoa(); //Criando a variavel e instanciando o Objeto Pessoa
+		Pessoa pessoa1 = new Pessoa(); //Criando a variavel e instanciando o Objeto Pessoa
 		
-		pessoal.nome = "Thamires";
-		pessoal.numfigurinhas = 1;
+		pessoa1.nome = "Thamires";
+		pessoa1.numfigurinhas = 1;
 		
-		pessoal.receber(3);
+		pessoa1.receber(3);
 		
-		System.out.println(pessoal.nome);
-		System.out.println(pessoal.numfigurinhas);
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numfigurinhas);
+		
+		Pessoa pessoa2 = new Pessoa();
+		
+		pessoa2.nome = "Thais";
+		pessoa2.numfigurinhas = 5;
+		
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.numfigurinhas);
 			
+		boolean msg = pessoa1.dar(1, pessoa2);
+		
+		if(msg==true)
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numfigurinhas);
+		
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.numfigurinhas);
+			
+		
+		
 
 	}
 
--- OrientacaoObjetos/src/Pessoa.java
+++ OrientacaoObjetos/src/Pessoa.java
@@ -2,12 +2,37 @@
 public class Pessoa {
      String nome;
      int numfigurinhas;
+    
      
      void receber(int numfigurinhas) {
     	 this.numfigurinhas += numfigurinhas;
     	 
      }
-     void dar () {
+     boolean dar(int numfigurinhas, Pessoa pessoa) {
+    	//boolean msg:
+    	 
+    	 this.numfigurinhas -= numfigurinhas;
+    	 
+    	 if(this.numfigurinhas < numfigurinhas) {
+    	 System.out.println("O número de figurinhas que voce tem é menor que a quantidade que voce quer dar");
+    	// msg = false;
+    	 return false;
+    	 
+    	 }else{
+    		 this.numfigurinhas -= numfigurinhas;
+    		 pessoa.receber(numfigurinhas);
+    		// msg = true;
+    		 return true;
+    		 
+    	 }
+    	 
+    	 
+    //	return msg;
+    	 
+    	 
+    	 
+    	 
+    	
     	 
      }
 }
