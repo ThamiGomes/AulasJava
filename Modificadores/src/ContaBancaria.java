a5f3d3ebf42b176344d7f788ebdd562ea4d94a0a
aluno <aluno@DSN1251230709>
2022-05-02 12:05:17 -0300

    Primeiro commit

--- Modificadores/src/ContaBancaria.java
+++ Modificadores/src/ContaBancaria.java
@@ -33,7 +33,7 @@ public class ContaBancaria {
 		
 	}
 
-	public ContaBancaria() {
+	{
 		
 	}
 }
--- Modificadores/src/Main.java
+++ Modificadores/src/Main.java
@@ -21,6 +21,49 @@ public class Main {
 		System.out.println(conta2.saldo());
 		
 		
+		Cliente cliente1= new Cliente();
+		
+		cliente1.setNome("Thami");
+		cliente1.setCpf(1234);
+		cliente1.setNumConta(1010);
+		
+		System.out.println(cliente1.getNome());
+		System.out.println(cliente1.getCpf());
+		System.out.println(cliente1.getNumConta());
+		
+    	System.out.println(cliente1);
+    	
+    	Cliente cliente2 = new Cliente();
+    	cliente2.setNome("Kaike");
+    	cliente2.setCpf(1515);
+    	cliente2.setNumConta(1234);
+    	
+//    	cliente2 = cliente1;
+    	
+    	System.out.println(cliente2);
+    	
+    	if(cliente1.getNome() == cliente2.getNome()) {
+    		System.out.println("Os nomes são iguais");
+    	
+    	} else {
+    		System.out.println("os nomes são diferentes");
+    	}
+    	
+    	if(cliente1 == cliente2) {
+    		System.out.println("os objetos são iguais");    		
+    	}else {
+    		System.out.println("os objetos são diferentes");
+    		
+    	}
+    	
+    	if(cliente1.equals(cliente2)) {
+    		System.out.println("Os atributosdos objetos são iguais");
+    		
+    	}else {
+    		System.out.println("Os atributos dos objetos são diferentes");
+    		
+    	}
+    		
 			
 	}
 
