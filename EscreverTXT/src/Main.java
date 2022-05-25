98e5ccc9b31937d9e4ab9894c05f7b5356843266
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-05-25 11:41:57 -0300

    Quinto Commit

--- EscreverTXT/src/Main.java
+++ EscreverTXT/src/Main.java
@@ -1,16 +1,28 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+
+	     Sabonete sabonete1 = new Sabonete("Gardênia" , "verde" );
+	     Sabonete sabonete2 = new Sabonete("Lavanda" , "Roxo" );
+	     Sabonete sabonete3 = new Sabonete("Morango" , "Rosa" );
+		
+		List<Sabonete> listaDeSabonetes = new ArrayList<Sabonete>();
+		
+		listaDeSabonetes.add(sabonete1);
+		listaDeSabonetes.add(sabonete2);
+		listaDeSabonetes.add(sabonete3);
 		
 		String s = "texto para ser gravado no arquivo";
 		
 		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("saida.txt"))){
-		
-		escrever.write(s);
+		   for(Sabonete sabonete : listaDeSabonetes)
+			   escrever.write(sabonete.toString());
 		
 		}
 	}
