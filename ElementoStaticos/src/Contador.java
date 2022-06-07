02ef6772e81bfce4b7d5a4b3cc0091fa8ff595e1
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 17:43:22 -0300

    Update Contador.java

--- ElementoStaticos/src/Contador.java
+++ ElementoStaticos/src/Contador.java
@@ -1,18 +1,20 @@
 
 public class Contador {
 
-	private static int valor;
+	private static int valor;  //variável static, ou seja, o valor de uma variável não varia
 	
 //	public Contador(int valor) {	
 //		this.valor = valor = 1;
 	
-	static {
-		valor = 1;
+	static { 
+	//variável static, o valor de uma variável não varia
+		valor = 1; // variavel inicia com o valor 1
 	}
 	public void incrementar() {
-		valor ++;
+		valor ++;  //incrementa em 1 a variável
 	}	
 
+	// gerando métodos getters and setters para acessar os atributos privados
 	public int getValor() {
 		return valor;
 	}
