3a0fe6c3ba8bd3a4be5c10d30f6349e9af0ef389
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 20:02:46 -0300

    Update Matematica.java

--- Sobrecarga/src/Matematica.java
+++ Sobrecarga/src/Matematica.java
@@ -1,18 +1,18 @@
 
 public class Matematica {
-	int somar(int x, int y) {
+	int somar(int x, int y) { //método somar com duas variáveis inteiras
 		
-		//int z = x + y;
-		//return z;
-		return x + y;
+		//int z = x + y; possibilidade de retorno
+		//return z; possibilidade de retorno
+		return x + y; //retorno da fórmula
 	}
 
-	int somar(int x, int y, int z) {
+	int somar(int x, int y, int z) { //método somar usando tres variáveis inteiras
 		
-		return x + y + z;
+		return x + y + z; //retorno da fórmula
 	}
-	double somar(double x, double y) {
+	double somar(double x, double y) { //método somar usando duas variáveis double
 		
-		return x + y;
+		return x + y; //retorno da fórmula
 	}
 }
