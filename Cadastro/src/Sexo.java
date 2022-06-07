cd9ba65866c27acebe14cab6eb888431077108df
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 16:27:01 -0300

    Update Sexo.java

--- Cadastro/src/Sexo.java
+++ Cadastro/src/Sexo.java
@@ -1,14 +1,15 @@
 
 public enum Sexo {
 
+	//definindo constantes que serão usadas pela classe Usuário
 	FEMININO(1),
 	MASCULINO(2);
   	   
    
 
-	private int num;
+	private int num; //variavel privada NUm
 
-	
+	//gerando getters and setters Num
 	public int getNum() {
 		return num;
 	}
