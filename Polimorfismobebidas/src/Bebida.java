23c23b21c88ad3d1586d846a15bae092e674993b
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 19:26:25 -0300

    Update Bebida.java

--- Polimorfismobebidas/src/Bebida.java
+++ Polimorfismobebidas/src/Bebida.java
@@ -1,17 +1,20 @@
 
 public class Bebida {
+	//atributos da super classe 
 	private String nome;
 	private boolean aquecer;
+	
+	//construtor que recebe os atributos
 	public Bebida(String nome, boolean aquecer) {
 		super();
 		this.nome = nome;
 		this.aquecer = aquecer;
 		
-	}
+	} //método preparar
 	public void preparar() {
 		
 	}
-	
+	// gerando getters and setters
 	public String getNome() {
 		return nome;
 	}
