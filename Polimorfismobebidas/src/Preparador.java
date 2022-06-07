4b2574b018ca31004bd1053daaf69163b5f9d0ca
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 19:35:54 -0300

    Update Preparador.java

--- Polimorfismobebidas/src/Preparador.java
+++ Polimorfismobebidas/src/Preparador.java
@@ -1,17 +1,17 @@
 
 public class Preparador {
 
-	public void prepararBebida(Bebida bebida) {
+	public void prepararBebida(Bebida bebida) { //método prepararBebida em que o parametro é a bebida que será preparada
 		
-		System.out.println("Preparando a bebida" + bebida.getNome());
-		System.out.println("Pegando copo");
+		System.out.println("Preparando a bebida" + bebida.getNome()); //Syso para mostrar na tela a bebida que esta sendo preparada
+		System.out.println("Pegando copo"); //mostrar na tela
 		
 		bebida.preparar();
-		if (bebida.isAquecer())
-			System.out.println("Aquecendo a bebida" + bebida.getNome()); {
+		if (bebida.isAquecer()) // if para saber se a bebida será aquecida
+			System.out.println("Aquecendo a bebida" + bebida.getNome()); { //Syso para mostrar na tela que  bebida esta sendo aquecida
 				
 			}
 			
-	    System.out.println("Bebida preparada com sucesso!!\n");
+	    System.out.println("Bebida preparada com sucesso!!\n"); //mostrar na tela que a bebida foi preparada
 	}
 }
