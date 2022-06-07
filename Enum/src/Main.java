db43badbc922f45b4b76ddb966578758aaff468f
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 17:34:49 -0300

    Update Main.java

--- Enum/src/Main.java
+++ Enum/src/Main.java
@@ -1,27 +1,28 @@
 
 public class Main {
 
-	public static final double PI = 3.14;
+	public static final double PI = 3.14; //determina que é constante
 	
-	private enum Sexo{
-		MASCULINO, FEMININO;
+	private enum Sexo{ //criando um Enum dentro da própria classe
+		MASCULINO, FEMININO; //definindo constantes
 	}
 	
 	
 	public static void main(String[] args) {
 		
 		double pi = Main.PI;
-		DiaDaSemana hoje= DiaDaSemana.SEGUNDA;
+		DiaDaSemana hoje= DiaDaSemana.SEGUNDA; //colocando o endereço do objeto SEGUNDA
 		
 		int num = DiaDaSemana.SEGUNDA.getNum();
 		System.out.println(num);
 
-		String s = "TERCA";
-        hoje = DiaDaSemana.valueOf(s);
+		String s = "TERCA"; //(s) correspondera a TERCA
+        hoje = DiaDaSemana.valueOf(s); //vai procurar Na lista de constantes "DiaDaSemana" um valor que corresponda a (s)
         
-        System.out.println(hoje.getNum());
+        System.out.println(hoje.getNum());  //Syso para motrar o número correspondente do dia
         
-        Sexo sexo = Sexo.FEMININO;
+	//criaçã de um Enum dentro da classe	
+        Sexo sexo = Sexo.FEMININO; 
         sexo= Sexo.MASCULINO;
    }
 
