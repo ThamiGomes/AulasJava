1ee8f415cd7092dc9247334a346731f987af1f90
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 17:50:23 -0300

    Update Aplicacao.java

--- ElementoStaticos/src/Aplicacao.java
+++ ElementoStaticos/src/Aplicacao.java
@@ -6,15 +6,15 @@ public class Aplicacao {
 
 	//	Matematica matematica = new Matematica();
 		
-	//	int resultado = matematica.somar(2, 3);
+		int resultado = matematica.somar(2, 3); //como os métodos são estáticos, não será preciso instanciar um objeto colocando o resultado da soma de dois números na variável 'resultado' ,o parâmetro são os 2 valores que serão somados
 	//	System.out.println(resultado);
 	//	resultado= matematica.subtrair(10, 4);
-	//	System.out.println(resultado);
+	//	System.out.println(resultado); //Syso para mostrar o resultado na tela
 		
 	//	int resultado = Matematica.somar(2, 4);
-	//	System.out.println(resultado);
-	//	resultado = Matematica .subtrair(10, 11);
-	//	System.out.println(resultado);
+		System.out.println(resultado);  //Syso para mostrar o resultado na tela
+		resultado = Matematica .subtrair(10, 11);
+		System.out.println(resultado);  //Syso para mostrar o resultado na tela
 		
 	//	Contador contador1 = new Contador() ;
 	//	Contador contador2 = new Contador() ;
@@ -29,7 +29,7 @@ public class Aplicacao {
 		
 	//	System.out.println(Contador.getValor());
 		
-		System.out.println(Constante.MEDIA_DA_PROVA);
+		System.out.println(Constante.MEDIA_DA_PROVA); //mostrando o valor da constante
 		
 	}
 
