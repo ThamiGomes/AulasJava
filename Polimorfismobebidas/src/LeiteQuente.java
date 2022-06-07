f97ec6b97667f2b1eb36b233de8a9a2571129adb
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 19:45:38 -0300

    Update LeiteQuente.java

--- Polimorfismobebidas/src/LeiteQuente.java
+++ Polimorfismobebidas/src/LeiteQuente.java
@@ -1,15 +1,15 @@
 
-public class LeiteQuente extends Bebida {
+public class LeiteQuente extends Bebida { //Classe LeiteQuente que extends da super classe Bebida
 
 	public LeiteQuente() {
-		super("LeiteQuente", true);
+		super("LeiteQuente", true); // O construtor estapegando informaçoes da super classe, dizendo o nome, e que é para a bebia ser aquecida
 		
 	}
 
 	@Override
-	public void preparar() {
+	public void preparar() { //usando metodo preparar da uper classe
 		
-		System.out.println("Colocando Leite Quente no copo");
+		System.out.println("Colocando Leite Quente no copo"); //mostrandoinformaçoes na tela
 		
 	}
 
