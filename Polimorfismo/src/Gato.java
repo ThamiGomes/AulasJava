8a533e07950f2739ec915d7a5e69190bfafc4c9a
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 19:18:38 -0300

    Update Gato.java

--- Polimorfismo/src/Gato.java
+++ Polimorfismo/src/Gato.java
@@ -1,7 +1,8 @@
 
-public class Gato extends Animais {
+public class Gato extends Animais { //classe Gato que extends de Animal
 
 	@Override
+	//metodo falar da classe animal
 	public void falar() {
 	System.out.println("MIAUU");
 		
