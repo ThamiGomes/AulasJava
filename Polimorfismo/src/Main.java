31310cea23d12bef7ee9cfe7144530c2219643e6
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 19:10:07 -0300

    Update Main.java

--- Polimorfismo/src/Main.java
+++ Polimorfismo/src/Main.java
@@ -10,14 +10,18 @@ public class Main {
 	//  animais.Falar();	
 		
 		
-		Gato gato= new Gato();
-		Cachorro cachorro= new Cachorro();
-		falar(gato);
+		Gato gato= new Gato(); //criando  um objeto do tipo gato na heap. A variável "gato" aponta para a classe Gato.
+		Cachorro cachorro= new Cachorro();  //criando  um objeto do tipo cachorro na heap. A variável "Cachorro" aponta para a classe Cachorro.
+		
+		//o método falar precisa de um parâmetro dentro de "Animal"
+		falar(gato); 
 		falar(cachorro);
 	}
 
-	public static void falar(Animais a) {
+	public static void falar(Animais a) { // o A representa um Animal
 		a.Falar();
+		
+		 // instanceof é um operador de comparação. A resposta será true ou será false. O  instanceof vê se o objeto é da classe que esta comparando.
 		
 		if(a instanceof Cachorro) {
 			Cachorro c = (Cachorro) a;
