01d96a7a8e2f881a60f8edaad9bb5ec212f04849
ThamiGomes <102962996+ThamiGomes@users.noreply.github.com>
2022-06-07 20:14:54 -0300

    Update Diamante.java

--- ClassesAbstratas/src/Diamante.java
+++ ClassesAbstratas/src/Diamante.java
@@ -1,9 +1,9 @@
 
-public class Diamante extends Item {
+public class Diamante extends Item { //subclasse que extends de item
 
 	@Override
 	public void pegar() {
-		System.out.println("Pegou Diamante");
+		System.out.println("Pegou Diamante"); // Syso para mostar mensagem na tela
 	}
 
 	
